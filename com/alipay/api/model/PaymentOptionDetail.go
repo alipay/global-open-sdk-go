91d85f3751c9a9fd264f30127af182cc0760a2a6
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-12-10 03:09:42 +0000

    [reformat][antom-sdk-automation] automated change

--- com/alipay/api/model/PaymentOptionDetail.go
+++ com/alipay/api/model/PaymentOptionDetail.go
@@ -4,4 +4,5 @@ type PaymentOptionDetail struct {
 	SupportCardBrands []*SupportCardBrand `json:"supportCardBrands,omitempty"`
 	Funding           []string            `json:"funding,omitempty"`
 	SupportBanks      []*SupportBank      `json:"supportBanks,omitempty"`
+	InteractionTypes  []string            `json:"interactionTypes,omitempty"`
 }
