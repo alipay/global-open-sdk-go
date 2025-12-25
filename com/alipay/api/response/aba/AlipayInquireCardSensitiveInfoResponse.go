6b57c99bfc6021a4d9748dd901721797aa1e2d60
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-12-25 03:33:03 +0000

    [reformat][antom-sdk-automation] automated change

---
+++ com/alipay/api/response/aba/AlipayInquireCardSensitiveInfoResponse.go
@@ -0,0 +1,16 @@
+package responseAba
+
+import (
+	"github.com/alipay/global-open-sdk-go/com/alipay/api/model"
+	"github.com/alipay/global-open-sdk-go/com/alipay/api/response"
+)
+
+type AlipayInquireCardSensitiveInfoResponse struct {
+	response.AlipayResponse
+	Result       *model.Result `json:"result,omitempty"`
+	AssetId      string        `json:"assetId,omitempty"`
+	Cvv          string        `json:"cvv,omitempty"`
+	CardNo       string        `json:"cardNo,omitempty"`
+	ExpiredMonth string        `json:"expiredMonth,omitempty"`
+	ExpiredYear  string        `json:"expiredYear,omitempty"`
+}
