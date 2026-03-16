fe648df9b6e5f75a4802e081d80f62755384dee7
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-03-16 12:10:28 +0000

    [reformat][antom-sdk-automation] automated change

--- com/alipay/api/request/subscription/AlipaySubscriptionUpdateRequest.go
+++ com/alipay/api/request/subscription/AlipaySubscriptionUpdateRequest.go
@@ -7,13 +7,15 @@ import (
 )
 
 type AlipaySubscriptionUpdateRequest struct {
-	SubscriptionUpdateRequestId string            `json:"subscriptionUpdateRequestId,omitempty"`
-	SubscriptionId              string            `json:"subscriptionId,omitempty"`
-	SubscriptionDescription     string            `json:"subscriptionDescription,omitempty"`
-	PeriodRule                  *model.PeriodRule `json:"periodRule,omitempty"`
-	PaymentAmount               *model.Amount     `json:"paymentAmount,omitempty"`
-	SubscriptionEndTime         string            `json:"subscriptionEndTime,omitempty"`
-	OrderInfo                   *model.OrderInfo  `json:"orderInfo,omitempty"`
+	SubscriptionUpdateRequestId string                   `json:"subscriptionUpdateRequestId,omitempty"`
+	SubscriptionId              string                   `json:"subscriptionId,omitempty"`
+	SubscriptionDescription     string                   `json:"subscriptionDescription,omitempty"`
+	PeriodRule                  *model.PeriodRule        `json:"periodRule,omitempty"`
+	PaymentAmount               *model.Amount            `json:"paymentAmount,omitempty"`
+	SubscriptionEndTime         string                   `json:"subscriptionEndTime,omitempty"`
+	OrderInfo                   *model.OrderInfo         `json:"orderInfo,omitempty"`
+	ProrationSettings           *model.ProrationSettings `json:"prorationSettings,omitempty"`
+	NextSubscriptionDate        string                   `json:"nextSubscriptionDate,omitempty"`
 }
 
 func NewAlipaySubscriptionUpdateRequest() (*request.AlipayRequest, *AlipaySubscriptionUpdateRequest) {
