11ee0820d0a8c85fd86fd6b562162000ef03db86
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-03-16 10:07:08 +0000

    [reformat][antom-sdk-automation] automated change

---
+++ com/alipay/api/response/aba/AlipayInquireDirectRefundResponse.go
@@ -0,0 +1,21 @@
+package responseAba
+
+import (
+	"github.com/alipay/global-open-sdk-go/com/alipay/api/model"
+	"github.com/alipay/global-open-sdk-go/com/alipay/api/response"
+)
+
+type AlipayInquireDirectRefundResponse struct {
+	response.AlipayResponse
+	RefundStatus        model.TransactionStatusType `json:"refundStatus,omitempty"`
+	RefundResultMessage string                      `json:"refundResultMessage,omitempty"`
+	RefundResultCode    string                      `json:"refundResultCode,omitempty"`
+	RefundId            string                      `json:"refundId,omitempty"`
+	PaymentId           string                      `json:"paymentId,omitempty"`
+	RefundRequestId     string                      `json:"refundRequestId,omitempty"`
+	RefundTime          string                      `json:"refundTime,omitempty"`
+	RefundFromMethod    *model.RefundFromMethod     `json:"refundFromMethod,omitempty"`
+	RefundToAmount      *model.Amount               `json:"refundToAmount,omitempty"`
+	RefundFromAmount    *model.Amount               `json:"refundFromAmount,omitempty"`
+	Result              *model.Result               `json:"result,omitempty"`
+}
