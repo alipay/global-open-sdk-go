f671b98506c1f3e05d2c72510610f63ff3a5340d
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-04-27 06:41:19 +0000

    [reformat][antom-sdk-automation] automated change

--- com/alipay/api/model/AuthorizationControl.go
+++ com/alipay/api/model/AuthorizationControl.go
@@ -1,11 +1,12 @@
 package model
 
 type AuthorizationControl struct {
-	CardActiveTime              string           `json:"cardActiveTime,omitempty"`
-	CardCancelTime              string           `json:"cardCancelTime,omitempty"`
-	AllowedMerchantCategoryList []string         `json:"allowedMerchantCategoryList,omitempty"`
-	AllowedAuthTimes            int32            `json:"allowedAuthTimes,omitempty"`
-	AllowedCurrencies           []string         `json:"allowedCurrencies,omitempty"`
-	CardLimitDetail             *CardLimitDetail `json:"cardLimitDetail,omitempty"`
-	CardLimitInfo               *CardLimitInfo   `json:"cardLimitInfo,omitempty"`
+	CardActiveTime              string            `json:"cardActiveTime,omitempty"`
+	CardCancelTime              string            `json:"cardCancelTime,omitempty"`
+	AllowedMerchantCategoryList []string          `json:"allowedMerchantCategoryList,omitempty"`
+	AllowedAuthTimes            int32             `json:"allowedAuthTimes,omitempty"`
+	AllowedCurrencies           []string          `json:"allowedCurrencies,omitempty"`
+	CardLimitDetail             *CardLimitDetail  `json:"cardLimitDetail,omitempty"`
+	CardLimitInfo               *CardLimitInfo    `json:"cardLimitInfo,omitempty"`
+	RefundPreference            *RefundPreference `json:"refundPreference,omitempty"`
 }
