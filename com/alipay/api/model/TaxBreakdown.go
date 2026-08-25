c06e40e5c6213c6ff27df19262c599e6268ce056
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-08-25 07:59:45 +0000

    [reformat][antom-sdk-automation] automated change

--- com/alipay/api/model/TaxBreakdown.go
+++ com/alipay/api/model/TaxBreakdown.go
@@ -1,11 +1,11 @@
 package model
 
 type TaxBreakdown struct {
-	TaxType          string `json:"taxType,omitempty"`
-	AuthorityName    string `json:"authorityName,omitempty"`
-	TaxRate          string `json:"taxRate,omitempty"`
-	TaxAmount        string `json:"taxAmount,omitempty"`
-	TaxableAmount    string `json:"taxableAmount,omitempty"`
-	TaxabilityReason string `json:"taxabilityReason,omitempty"`
-	Inclusive        bool   `json:"inclusive,omitempty"`
+	TaxType          string  `json:"taxType,omitempty"`
+	AuthorityName    string  `json:"authorityName,omitempty"`
+	TaxRate          string  `json:"taxRate,omitempty"`
+	TaxAmount        *Amount `json:"taxAmount,omitempty"`
+	TaxableAmount    *Amount `json:"taxableAmount,omitempty"`
+	TaxabilityReason string  `json:"taxabilityReason,omitempty"`
+	Inclusive        bool    `json:"inclusive,omitempty"`
 }
--- com/alipay/api/model/TaxId.go
+++ com/alipay/api/model/TaxId.go
@@ -4,5 +4,4 @@ type TaxId struct {
 	Value   string `json:"value,omitempty"`
 	Country string `json:"country,omitempty"`
 	Region  string `json:"region,omitempty"`
-	Name    string `json:"name,omitempty"`
 }
