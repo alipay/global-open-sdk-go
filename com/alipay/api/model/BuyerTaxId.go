8e3061e197406189b5bd774169019b6e6110150a
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-09-09 09:14:54 +0000

    [reformat][antom-sdk-automation] automated change

--- com/alipay/api/model/BuyerTaxId.go
+++ com/alipay/api/model/BuyerTaxId.go
@@ -1,7 +1,7 @@
 package model
 
-type TaxJurisdiction struct {
+type BuyerTaxId struct {
 	Country string `json:"country,omitempty"`
 	Region  string `json:"region,omitempty"`
-	City    string `json:"city,omitempty"`
+	Value   string `json:"value,omitempty"`
 }
